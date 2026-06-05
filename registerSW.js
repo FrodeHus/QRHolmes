d2a79bfc9286926f3b3933852ade775862b652d6
frodehus <frode.hus@outlook.com>
2026-06-05 14:20:14 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/QRHolmes/sw.js', { scope: '/QRHolmes/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
